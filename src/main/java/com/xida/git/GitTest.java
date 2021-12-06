25adc52c921601f47505e1175130adf38619b8ff
Sun <2441234838@qq.com>
2021-12-06 16:48:55 +0800

    调整版式 commit

--- src/main/java/com/xida/git/GitTest.java
+++ src/main/java/com/xida/git/GitTest.java
@@ -7,17 +7,15 @@ package com.xida.git;
 public class GitTest {
 
     public static void main(String[] args) {
+
         System.out.println("你好 Git!");
         System.out.println("hello Git!");
         System.out.println("你好 Git3");
         System.out.println("你好 Git!44");
         System.out.println("master test");
-
         System.out.println("hot-fix test");
         System.out.println("push test");
-
         System.out.println("pull test");
-
 
 
     }
