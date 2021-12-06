13ed11800089b96ba85519b9e8e79f2b0d9848a5
Sun <2441234838@qq.com>
2021-12-06 15:30:19 +0800

    first commit

---
+++ src/main/java/com/xida/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.xida.git;
+
+/**
+ * @author sun
+ * @create 2021-12-06-15:22
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("你好 Git!");
+    }
+
+}
